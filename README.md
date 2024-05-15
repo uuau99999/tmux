aedb77047817e38e7bad92b54966d6e7d752d096
uuau99999 <uuau99999@gmail.com>
2024-05-15 10:55:12 +0800

    chore: update tmux.conf

---
+++ README.md
@@ -0,0 +1,7 @@
+### Tmux Configuration
+
+# Usage
+
+1. Run ./prepare.sh
+2. Enter tmux
+3. Run Ctrl-B + I to install plugins
---
+++ prepare.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+ln -s ~/.config/tmux/tmux-clear ~/.local/bin/tmux-clear
+ln -s ~/.config/tmux/tmux-sessionizer ~/.local/bin/tmux-sessionizer
