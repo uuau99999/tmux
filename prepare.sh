d99ea63271c7f291ad26a80a5e2227c2a3db0520
uuau99999 <uuau99999@gmail.com>
2024-05-17 20:06:41 +0800

    chore: update prepare.sh and README.md

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+mkdir -p ~/.local/bin
 ln -s ~/.config/tmux/tmux-clear ~/.local/bin/tmux-clear
 ln -s ~/.config/tmux/tmux-sessionizer ~/.local/bin/tmux-sessionizer
+
+touch ~/.tmux-repo
+echo ~/.config/tmux >>~/.tmux-repo
